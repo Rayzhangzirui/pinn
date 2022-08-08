eb1e6c3deaf684efdc3748c8387ae4d14cc9ef41
rzhang <rzhang@gru.ics.uci.edu>
2022-08-08 01:58:35 -0700

    Separate model definition to simple.py Modify callback to print parameters

--- pinn.py
+++ pinn.py
@@ -1,68 +1,17 @@
 #!/usr/bin/env python
 # coding: utf-8
-from cmath import nan
 import math
-from config import *
 import tensorflow as tf
 import numpy as np
 from time import time
 import tensorflow_probability as tfp
 import scipy.optimize
-import os
+
 from datetime import datetime
+from config import *
 from util import *
 
-os.environ['CUDA_VISIBLE_DEVICES'] = "0"
 
-DIM=1 # dimension of the problem
-epsilon = 0.01 # width of diffused domain
-T = 300
-D = 0.13e-4
-rho = 0.025
-bound = 0.7 #bound in xy plane
-
-test_case = True
-# tf.config.run_functions_eagerly(True)
-
-
-# paths
-model_name = "test"
-timetag = datetime.now().isoformat(timespec='minutes')
-dirname = f"{model_name}:{timetag}"
-MD_SAVE_DIR = os.path.join(DATADIR,dirname)
-# mkdir if not exist
-if not test_case:
-    os.makedirs(MD_SAVE_DIR,exist_ok=False)
-
-if test_case:
-    tf.random.set_seed(1234)
-    num_init_train = 100 # initial traning iteration
-    n_res_pts = 100 # number of residual point
-    n_dat_pts = 1000 # number of data points
-    n_test_points = 100 # number of testing point
-    num_hidden_unit = 4 # hidden unit in one layer
-    num_add_pt = 10 # number of anchor point
-    max_adpt_step = 1 # number of adaptive sampling
-    num_adp_train = 1000 #adaptive adam training step
-    print_res_every = 100 # print residual
-    save_res_every = None # save residual
-    w_dat = 0.5 # weight of data, weight of res is 1-w_dat
-    model_name = "test"
-    
-else:
-    num_init_train = 20000
-    n_res_pts = 10000
-    n_dat_pts = 10000
-    n_test_points=10**(DIM+1)
-    num_hidden_unit = 100
-    num_add_pt = 1000
-    max_adpt_step = 0
-    num_adp_train = 1000
-    print_res_every = 1000
-    save_res_every = 100
-    w_dat = 0
-
-lbfgs_opt = {"maxcor": 100, "ftol": 0, "gtol": 1e-8, "maxiter": 10000, "maxls": 50}
 
 
 # domain = [[0., 1.],[-0.7,0.7]]
@@ -91,46 +40,6 @@ lbfgs_opt = {"maxcor": 100, "ftol": 0, "gtol": 1e-8, "maxiter": 10000, "maxls": 
 
 # u_exact = None
 
-param = tf.Variable([0., 0.])
-param_true = tf.constant([2., 1.0])
-DIM=1 
-
-domain = [[0., 1.]]
-
-# def output_transform(x,u):
-#     return tf.math.sin(math.pi * x) * u
-
-def output_transform(x,u):
-    return u
-
-def pde(x_r, f):
-    x = x_r
-    u =  f(x)
-    
-    u_x = tf.gradients(u, x)[0]
-    # u_xx = tf.gradients(u_x, x)[0]
-
-    return u_x-(f.param[0]*x+f.param[1])
-
-def u_exact(x):
-    return param_true[0]*x**2/2 + param_true[1]*x
-
-
-
-
-# Draw uniformly sampled collocation points
-x_r = sample(n_res_pts, domain)
-
-
-x_dat = None
-u_dat = None
-if w_dat > 0:
-    x_dat = x_r
-    if u_exact is not None:
-        u_dat = u_exact(x_dat)
-    # u_dat = interpsol('sol1d.txt', 100, 100, x_dat)
-
-
 
 # Define model architecture
 class PINN_NeuralNet(tf.keras.Model):
@@ -142,14 +51,15 @@ class PINN_NeuralNet(tf.keras.Model):
             num_neurons_per_layer=100,
             activation='tanh',
             kernel_initializer='glorot_normal',
-            output_transform = output_transform,
+            output_transform = lambda x,u:u,
+            train_param = None,
             **kwargs):
         super().__init__(**kwargs)
 
         self.num_hidden_layers = num_hidden_layers
         self.output_dim = output_dim
         
-        self.param = param
+        self.train_param = train_param
 
         # Define NN architecture
         self.hidden = [tf.keras.layers.Dense(num_neurons_per_layer,
@@ -241,18 +151,22 @@ class LossAndFlatGradient(object):
     
 
 class PINNSolver():
-    def __init__(self, model, x_r, w_dat, x_dat=None, u_dat=None, u_exact = None):
+    def __init__(self, model, pde, x_r, x_dat=None, u_dat=None, u_exact = None, options=None, lbfgs_opt = None):
         self.model = model
         
         # Store collocation points
         self.x_r = x_r
+        
+        self.pde = pde
 
         # if exact sol is provided
         self.u_exact = u_exact
         
+        self.options = options
+
         
         # Initialize history of losses and global iteration counter
-        self.w_dat = w_dat
+        self.w_dat = options["w_dat"]
         self.hist = []
         self.iter = 0
 
@@ -260,7 +174,7 @@ class PINNSolver():
     def loss_fn(self, x_dat, u_dat):
         
         # Compute phi_r
-        r = pde(self.x_r, self.model)
+        r = self.pde(self.x_r, self.model)
         loss_res = tf.reduce_mean(tf.square(r))
         
         # Initialize loss
@@ -430,14 +344,7 @@ class PINNSolver():
             func,
             initial_position,
             previous_optimizer_results=None,
-            num_correction_pairs=lbfgs_opt['maxcor'],
-            tolerance=lbfgs_opt['gtol'],
-            x_tolerance=0,
-            f_relative_tolerance=lbfgs_opt['ftol'],
-            initial_inverse_hessian_estimate=None,
-            max_iterations=lbfgs_opt['maxiter'],
-            parallel_iterations=1,
-            max_line_search_iterations=lbfgs_opt['maxls']
+            **kwargs
         )
         
         loss_final = results.objective_value.numpy()
@@ -453,7 +360,7 @@ class PINNSolver():
         scipy.optimize.minimize require first arg to be parameters 
         """
         
-        if self.iter % print_res_every == 0:
+        if self.iter % self.options["print_res_every"] == 0:
             str_loss = '[{:10.4e}, {:10.4e}, {:10.4e}] '.format(self.current_loss['res'],self.current_loss['data'],self.current_loss['total'])
             # if exact solution is provided, 
             if self.u_exact:
@@ -466,11 +373,11 @@ class PINNSolver():
 
 
         # save residual to file with interval save_res_every
-        if save_res_every is not None and self.iter % save_res_every == 0:
+        if self.options["save_res_every"] is not None and self.iter % self.options["save_res_every"] == 0:
             fname = f"data{self.iter}.dat"
             u = self.model(self.x_r)
             data = tf.concat([self.x_r, u, self.current_res],1)
-            np.savetxt( os.path.join(MD_SAVE_DIR,fname) , data.numpy())
+            np.savetxt( os.path.join( self.options["model_dir"], fname) , data.numpy())
 
         # loss history
         self.hist.append(self.current_loss)
@@ -479,34 +386,3 @@ class PINNSolver():
             
         
 
-
-# Initialize model
-model = PINN_NeuralNet()
-model.build(input_shape=(None,DIM))
-
-# Initilize PINN solver
-solver = PINNSolver(model, x_r, w_dat, u_exact = u_exact)
-
-# Start timer
-t0 = time()
-
-# different learning rate schedules
-# lr = tf.keras.optimizers.schedules.PiecewiseConstantDecay([1000,3000],[1e-2,1e-3,5e-4])
-lr = tf.keras.optimizers.schedules.PolynomialDecay(1e-2,decay_steps=num_init_train,end_learning_rate=1e-4)
-
-
-optim = tf.keras.optimizers.Adam(learning_rate=lr)
-solver.solve_with_TFoptimizer(optim, x_dat,u_dat, N=num_init_train)
-    
-
-# results = solver.solve_with_ScipyOptimizer(x_dat,u_dat,method='L-BFGS-B',options=lbfgs_opt)
-
-results = solver.solve_with_tfbfgs(x_dat,u_dat)
-
-# Print computation time
-print('\nComputation time: {} seconds'.format(time()-t0))
-
-
-
-if not test_case:
-    model.save(os.path.join(MD_SAVE_DIR,'savemodel'))
