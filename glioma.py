b3deb8ed8c0c3ad25f1512055003b5f8ffcdd40c
ray <raycheunghku@gmail.com>
2022-11-13 20:07:58 -0800

    Investigate using polar and exact residual
    Change pwm, pgm data to single P
    Change Dphi to Pphi
    Add exactres pde as option

--- glioma.py
+++ glioma.py
@@ -65,16 +65,14 @@ class DataSet:
 
         # residual pts
         self.xr =  matdat.get('xr')[0:nres,:]
+        self.P =  matdat.get('Pq')[0:nres,:]
         self.phi =  matdat.get('phiq')[0:nres,:]
-        self.pwm =  matdat.get('Pwmq')[0:nres,:]
-        self.pgm =  matdat.get('Pgmq')[0:nres,:]
-        self.DxDphi =  matdat.get('DxDphi')[0:nres,:]* self.DW 
-        self.DyDphi =  matdat.get('DyDphi')[0:nres,:]* self.DW 
-        self.DzDphi =  matdat.get('DzDphi')[0:nres,:]* self.DW 
+        self.DxPphi =  matdat.get('DxPphi')[0:nres,:]
+        self.DyPphi =  matdat.get('DyPphi')[0:nres,:]
+        self.DzPphi =  matdat.get('DzPphi')[0:nres,:]
 
 
         # characteristic diffusion ceofficient at each point
-        self.Dphi = self.DW * (self.pwm  + self.pgm * (1.0/10.0)) * self.phi
         self.dim = self.xr.shape[1]
         self.xdim = self.xr.shape[1]-1
 
@@ -124,31 +122,57 @@ class Gmodel:
                 return u* x[:, 0:1]+ ic(x)
 
         if self.xdim == 2:
-            @tf.function
-            def pde(x_r, f):
-                t = x_r[:,0:1]
-                x = x_r[:,1:2]
-                y = x_r[:,2:3]
-                xr = tf.concat([t,x,y], axis=1)
-                
-                u =  f(xr)
-                
-                u_t = tf.gradients(u, t)[0]
+            if self.opts.get('exactres') == True:
+                print('use exact residual')
+                @tf.function
+                def pde(x_r, f):
+                    t = x_r[:,0:1]
+                    x = x_r[:,1:2]
+                    y = x_r[:,2:3]
+                    xr = tf.concat([t,x,y], axis=1)
+                    
+                    r = tf.sqrt((x*self.dataset.L)**2+(y*self.dataset.L)**2)
+                    phi = 0.5 + 0.5*tf.tanh((50.0 - r)/3.0)
+                    D = f.param['rD'] * self.dataset.DW  *(0.9*( 0.5 + 0.5*tf.tanh((20.0 - r))) + 0.1)
+                    Dphi = D * phi
 
-                u_x = tf.gradients(u, x)[0]
-                u_y = tf.gradients(u, y)[0]
-                
-                # u_xx = tf.gradients(self.dataset.Dphi*u_x, x)[0]
-                # u_yy = tf.gradients(self.dataset.Dphi*u_y, y)[0]
+                    u =  f(xr)
+                    
+                    phiu_t = tf.gradients(phi*u, t)[0]
 
-                u_xx = tf.gradients(u_x, x)[0]
-                u_yy = tf.gradients(u_y, y)[0]
+                    u_x = tf.gradients(u, x)[0]
+                    u_y = tf.gradients(u, y)[0]
+                    
+                    u_xx = tf.gradients(Dphi * u_x, x)[0]
+                    u_yy = tf.gradients(Dphi * u_y, y)[0]
 
-                prolif = f.param['rRHO'] * self.dataset.RHO * self.dataset.phi * u * (1-u)
+                    res = phiu_t - ((u_xx + u_yy) + f.param['rRHO'] * self.dataset.RHO * phi * u * (1-u))
+                    return res
+            else:
+                @tf.function
+                def pde(x_r, f):
+                    t = x_r[:,0:1]
+                    x = x_r[:,1:2]
+                    y = x_r[:,2:3]
+                    xr = tf.concat([t,x,y], axis=1)
+                    
+                    u =  f(xr)
+                    
+                    u_t = tf.gradients(u, t)[0]
 
-                diffusion = f.param['rD'] * (self.dataset.Dphi * (u_xx + u_yy) + self.dataset.DxDphi * u_x + self.dataset.DyDphi * u_y)
-                res = self.dataset.phi * u_t - ( diffusion +  prolif)
-                return res
+                    u_x = tf.gradients(u, x)[0]
+                    u_y = tf.gradients(u, y)[0]
+                    
+                    u_xx = tf.gradients(u_x, x)[0]
+                    u_yy = tf.gradients(u_y, y)[0]
+
+                    prolif = f.param['rRHO'] * self.dataset.RHO * self.dataset.phi * u * (1-u)
+
+                    diffusion = f.param['rD'] * self.dataset.DW * (self.dataset.P *self.dataset.phi * (u_xx + u_yy) + self.dataset.DxPphi * u_x + self.dataset.DyPphi * u_y)
+                    res = self.dataset.phi * u_t - ( diffusion +  prolif)
+                    return res
+
+            
             
         else:
             @tf.function
