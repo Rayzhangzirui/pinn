40761f6ea2990564960f0667616b87a7cdcd84d1
Zirui Zhang <ziruz16@lucy.ics.uci.edu>
2023-02-13 12:41:56 -0800

    loaddetai.py: output result of each neuron

---
+++ loaddetail.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+# load model and make prediction
+
+import sys
+sys.path.insert(1, '/home/ziruz16/pinn')
+
+from config import *
+from glioma import *
+import json
+
+if __name__ == "__main__":
+    
+    optfile = sys.argv[1] #option file, used to load model
+    
+    # Opening JSON file
+    with open(optfile) as json_file:
+        opts = json.load(json_file)
+
+    opts['file_log'] = None    
+
+    for k, v in opts.items():
+        print(k, v)
+    
+    # restore from ckpt-1
+    opts['restore'] = 1
+    g = Gmodel(opts)
+    xrpred, A, b = g.model.lastlayer(g.dataset.xr)
+
+    xdatpred, _, _ = g.model.lastlayer(g.dataset.xdat)
+
+    savedat = {}
+    savedat['xrpred'] = [t2n(v) for v in xrpred]
+    savedat['xdatpred'] = [t2n(v) for v in xdatpred]
+    savedat['A'] = t2n(A)
+    savedat['b'] = t2n(b)
+
+    predfile = os.path.join(opts['model_dir'],f'detail_last_layer.mat')
+    print(f'save upred to {predfile}')
+    savemat(predfile,savedat)
+    
