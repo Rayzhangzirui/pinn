d6d8e4636b27c514c9eff18345abbf2716560689
Zirui Zhang <ziruz16@poison.ics.uci.edu>
2023-02-14 22:19:54 -0800

    Separate LossAndFlatGradient into tflbfgs.py file

--- pinn.py
+++ pinn.py
@@ -133,77 +133,8 @@ class PINN(tf.keras.Model):
         return Zout, self.out.weights[0], self.out.weights[1]
 
 
-
-
-# copy from 
-# https://github.com/lululxvi/deepxde/blob/9f0d86dea2230478d8735615e2ad518c62efe6e2/deepxde/optimizers/tensorflow/tfp_optimizer.py#L103
-class LossAndFlatGradient(object):
-    """A helper class to create a function required by tfp.optimizer.lbfgs_minimize.
-    Args:
-        trainable_variables: Trainable variables.
-        build_loss: A function to build the loss function expression.
-    """
-
-    def __init__(self, trainable_variables, build_loss):
-        self.trainable_variables = trainable_variables
-        self.build_loss = build_loss
-
-        # Shapes of all trainable parameters
-        self.shapes = tf.shape_n(trainable_variables)
-        self.n_tensors = len(self.shapes)
-
-        # Information for tf.dynamic_stitch and tf.dynamic_partition later
-        count = 0
-        self.indices = []  # stitch indices
-        self.partitions = []  # partition indices
-        for i, shape in enumerate(self.shapes):
-            n = np.product(shape)
-            self.indices.append(
-                tf.reshape(tf.range(count, count + n, dtype=tf.int32), shape)
-            )
-            self.partitions.extend([i] * n)
-            count += n
-        self.partitions = tf.constant(self.partitions)
-
-    @tf.function
-    def __call__(self, weights_1d):
-        """A function that can be used by tfp.optimizer.lbfgs_minimize.
-        Args:
-           weights_1d: a 1D tf.Tensor.
-        Returns:
-            A scalar loss and the gradients w.r.t. the `weights_1d`.
-        """
-        # Set the weights
-        self.set_flat_weights(weights_1d)
-        with tf.GradientTape() as tape:
-            # Calculate the loss
-            loss = self.build_loss()
-        # Calculate gradients and convert to 1D tf.Tensor
-        grads = tape.gradient(loss, self.trainable_variables)
-        grads = tf.dynamic_stitch(self.indices, grads)
-        return loss, grads
-
-    def set_flat_weights(self, weights_1d):
-        """Sets the weights with a 1D tf.Tensor.
-        Args:
-            weights_1d: a 1D tf.Tensor representing the trainable variables.
-        """
-        weights = tf.dynamic_partition(weights_1d, self.partitions, self.n_tensors)
-        for i, (shape, weight) in enumerate(zip(self.shapes, weights)):
-            self.trainable_variables[i].assign(tf.reshape(weight, shape))
-
-    def to_flat_weights(self, weights):
-        """Returns a 1D tf.Tensor representing the `weights`.
-        Args:
-            weights: A list of tf.Tensor representing the weights.
-        Returns:
-            A 1D tf.Tensor representing the `weights`.
-        """
-        return tf.dynamic_stitch(self.indices, weights)
-
-
 class PINNSolver():
-    def __init__(self, model, pde, 
+    def __init__(self, model, geomodel, pde, 
                 flosses,
                 ftests,
                 wr = None,
@@ -212,7 +143,7 @@ class PINNSolver():
                 xtest = None,
                 options=None):
         self.model = model
-        
+        self.geomodel = geomodel
         self.pde = pde
         
         self.flosses = flosses
@@ -263,21 +194,20 @@ class PINNSolver():
             sys.stdout = Logger(logfile)
         else:
             print('skip file log')
-            
+        
 
         # set up check point
         self.checkpoint = tf.train.Checkpoint(model)
-        self.manager = tf.train.CheckpointManager(self.checkpoint, directory=os.path.join(options['model_dir'],'ckpt'), max_to_keep=4)
+        self.manager = tf.train.CheckpointManager(self.checkpoint, directory=os.path.join(self.options['model_dir'],'ckpt'), max_to_keep=4)
         # self.manager.latest_checkpoint is None if no ckpt found
-        
-        
-        if options['restore'] is not None:
-            if isinstance(options['restore'],int):
+
+        if self.options['restore'] is not None:
+            if isinstance(self.options['restore'],int):
                 # restore check point in the same directory by integer, 0 = ckpt-1
-                ckptpath = self.manager.checkpoints[options['restore']]
+                ckptpath = self.manager.checkpoints[self.options['restore']]
             else:
                 # restore checkpoint by path
-                ckptpath = options['restore']
+                ckptpath = self.options['restore']
             self.checkpoint.restore(ckptpath)
             print("Restored from {}".format(ckptpath))
         else:
@@ -289,6 +219,7 @@ class PINNSolver():
             else:
                 print("No restore")
 
+        
         if self.options['trainnnweight'] == False:
             print("do not train NN")
             # self.model.trainable = False
@@ -309,7 +240,6 @@ class PINNSolver():
                 k += 1
 
         self.model.summary()
-
 
     @tf.function
     def loss_fn(self):
